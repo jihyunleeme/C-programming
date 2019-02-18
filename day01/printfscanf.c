330e4158843d3e58ad945fb2a9f02151d24c8037
jihyunlee <jhl7785@gmail.com>
2019-02-18 19:00:18 +0900

    printf and scanf

--- day01/printfscanf.c
+++ day01/printfscanf.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
-int main(void)
-{
+int main(void) 
+{	
+	/*
+
+	//정수형 변수에 대한 예제
 	int age = 12;
 	printf("%d\n", age);
+	
+	age = 13;
+	printf("%d\n", age);
+	//printf("2\n");
+
+	return 0;s
+
+	*/
+
+
+	//실수형 변수에 대항 예제
+	
+	float f = 46.4f;
+	printf("%.2f\n", f);
 	return 0;
 
-	/*
-	setting error
-	PDB 파일을 찾거나 열 수 없습니다.
-	
-	Debug
-	/*
 }
+
+/*
+setting error
+PDB 파일을 찾거나 열 수 없습니다.
+
+menu bar
+Debug > options > debugging > 기호 > Microsoft 기호 서버 체크해주기
+
+*/
